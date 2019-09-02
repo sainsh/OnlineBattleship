377e9aff48760b3f17a6b7e345f9a7e6361eae6d
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-02 09:44:22 +0100

    Coordinate done

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,6 +1,16 @@
 package Model;
 
-public class Game {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+public class Game {
+    Socket player1;
+    ObjectOutputStream outP1;
+    ObjectInputStream inP1;
+    Socket player2;
+    ObjectOutputStream outP2;
+    ObjectInputStream inP2;
+    Board board;
     //Board
 }
