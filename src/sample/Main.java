1a5ce5324f9fdb9e9ae7a39202a7bfe76a8b33ce
sainsh <kfsainsh@gmail.com>
2019-09-02 09:17:49 +0200

    removed sample.fxml changed size of window changed name from forlag...slagskib.fxml to clientView.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("clientView.fxml"));
+        primaryStage.setTitle("Sænk et Slagskib");
+        primaryStage.setScene(new Scene(root, 700, 1000));
         primaryStage.show();
     }
 
