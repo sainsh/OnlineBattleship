29af9aa7e9c0f9df62346c2741dece2bd03aecf3
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-02 09:40:13 +0100

    Coordinate done

--- src/Model/Coordinate.java
+++ src/Model/Coordinate.java
@@ -2,6 +2,40 @@ package Model;
 
 public class Coordinate {
 
+    int x;
+    int y;
+    boolean isHit;
+
+    public Coordinate(int x, int y, boolean isHit) {
+        this.x = x;
+        this.y = y;
+        this.isHit = isHit;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isHit() {
+        return isHit;
+    }
+
+    public void setHit(boolean hit) {
+        isHit = hit;
+    }
+
     //x: int
     //y: int
     //isHit: Boolean
