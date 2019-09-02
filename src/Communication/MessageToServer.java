7b26300d6abde611038718c2e2cf7153baa8bf95
sainsh <kfsainsh@gmail.com>
2019-09-02 10:38:29 +0200

    added variables, constructor, getters and setters to MessageToServer

--- src/Communication/MessageToServer.java
+++ src/Communication/MessageToServer.java
@@ -1,4 +1,58 @@
 package Communication;
 
 public class MessageToServer {
+
+    boolean isChatMessage = false;
+    boolean isShot = false;
+    String chatMessage;
+    Cell shot;
+    String sender;
+
+    public MessageToServer(boolean isChatMessage, boolean isShot, String chatMessage, Cell shot, String sender) {
+        this.isChatMessage = isChatMessage;
+        this.isShot = isShot;
+        this.chatMessage = chatMessage;
+        this.shot = shot;
+        this.sender = sender;
+    }
+
+    public boolean isChatMessage() {
+        return isChatMessage;
+    }
+
+    public void setChatMessage(boolean chatMessage) {
+        isChatMessage = chatMessage;
+    }
+
+    public boolean isShot() {
+        return isShot;
+    }
+
+    public void setShot(boolean shot) {
+        isShot = shot;
+    }
+
+    public String getChatMessage() {
+        return chatMessage;
+    }
+
+    public void setChatMessage(String chatMessage) {
+        this.chatMessage = chatMessage;
+    }
+
+    public Cell getShot() {
+        return shot;
+    }
+
+    public void setShot(Cell shot) {
+        this.shot = shot;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 }
