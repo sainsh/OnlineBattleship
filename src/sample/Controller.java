b66b1bd54058a88d20143607a51214b10daaf0df
sainsh <kfsainsh@gmail.com>
2019-09-02 11:11:27 +0200

    added chat functionality added send shot functionality

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,8 @@ import Communication.Connector;
 import Communication.MessageToClient;
 import Communication.MessageToServer;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -13,8 +15,21 @@ public class Controller {
     Connector connector;
     Thread thread;
 
+    int coordinateSize = 10;
+    int clickedX;
+    int clickedY;
+
     @FXML
-    public void initialize(){
+    TextField chatText;
+    @FXML
+    TextArea chatHistoryText;
+
+    Board playerBoard;
+    Board enemyBoard;
+
+
+    @FXML
+    public void initialize() {
         connector = new Connector(this);
         thread = new Thread(connector);
         thread.start();
@@ -24,6 +39,15 @@ public class Controller {
 
     public void onMouseClickedEnemyBoard(MouseEvent mouseEvent) {
         if (mouseEvent.isPrimaryButtonDown()) {
+            clickedX = (int)mouseEvent.getX()/coordinateSize;
+            clickedY = (int)mouseEvent.getY()/coordinateSize;
+
+
+            MessageToServer messageToServer = new MessageToServer();
+
+            messageToServer.setShot(true);
+            messageToServer.setShot(enemyBoard.getCell(clickedX,clickedY));
+            connector.send(messageToServer);
 
         }
     }
@@ -36,13 +60,25 @@ public class Controller {
 
     public void send() {
         MessageToServer messageToServer = new MessageToServer();
+        messageToServer.setChatMessage(true);
+        messageToServer.setChatMessage(chatText.getText());
+        chatHistoryText.appendText(chatText.getText() + "\n");
+        chatText.setText("");
 
         connector.send(messageToServer);
     }
 
     public void respondToMessage(MessageToClient messageToClient) {
+        if(messageToClient.isMessage()){
+            chatHistoryText.appendText(messageToClient.getMessage() + "\n");
+        }
 
+        if(messageToClient.isShot()){
+            draw(messageToClient);
+        }
 
+    }
 
+    private void draw(MessageToClient messageToClient) {
     }
 }
