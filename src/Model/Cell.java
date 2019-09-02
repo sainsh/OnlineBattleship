c7440f9d27f2302b31d56c1c5f436c9386ce4369
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-02 10:00:11 +0100

    Coordinate done

--- src/Model/Cell.java
+++ src/Model/Cell.java
@@ -1,8 +1,38 @@
 package Model;
 
 public class Cell {
+    private Coordinate coordinate;
 
-    //Coordinate
-    //Status: int
-    //Ship: ship
+    //Status 1 is not hit, status 2 is hit, status 3 is hit with a ship on it.
+    private int Status = 1;
+    private Ship ship;
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public int getStatus() {
+        if (ship != null){
+            setStatus(3);
+        }
+        else
+            setStatus(2);
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
 }
