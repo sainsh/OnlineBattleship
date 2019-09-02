8d8794e26e9beda43ee443d8ff49df208846d518
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-02 10:31:46 +0100

    Working on Board

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,30 +1,40 @@
 package Model;
 
 public class Board {
-    private Cell[][] Board1;
-    private Cell[][] Board2;
+    private Cell[][] board1;
+    private Cell[][] board2;
 
     public void setShipBoard1(Ship ship){}
 
     public void setShipBoard2(Ship ship){}
 
-    public void shotBoard1(int x, int y){}
+    public Cell shootBoard1(int x, int y){
+        Cell shotCell = board1[x][y];
+        shotCell.getCoordinate().setHit(true);
+        shotCell.getStatus();
+        return shotCell;
+    }
 
-    public void shotBoard2(int x, int y){}
+    public Cell shootBoard2(int x, int y){
+        Cell shotCell = board2[x][y];
+        shotCell.getCoordinate().setHit(true);
+        shotCell.getStatus();
+        return shotCell;
+    }
 
     public Cell[][] getBoard1() {
-        return Board1;
+        return board1;
     }
 
     public void setBoard1(Cell[][] board1) {
-        Board1 = board1;
+        this.board1 = board1;
     }
 
     public Cell[][] getBoard2() {
-        return Board2;
+        return board2;
     }
 
     public void setBoard2(Cell[][] board2) {
-        Board2 = board2;
+        this.board2 = board2;
     }
 }
