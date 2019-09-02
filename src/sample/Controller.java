c3f3379b3c5f709c1e0cfc02cedbd5d26b019ebc
sainsh <kfsainsh@gmail.com>
2019-09-02 09:41:39 +0200

    added methods from clientView to Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+
+    public void onMouseClickedEnemyBoard(MouseEvent mouseEvent) {
+        if(mouseEvent.isPrimaryButtonDown()){
+
+        }
+    }
+
+    public void onKeyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            send();
+        }
+    }
+
+    public void send() {
+    }
 }
